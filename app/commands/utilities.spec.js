b3c1ec56b63868d05961f7ee7c8262b4cfac944e
Marcos Tulli <marcos.tulli@raona.com>
2023-11-01 17:03:46 -0300

    Add tests for emmpty

--- app/commands/utilities.spec.js
+++ app/commands/utilities.spec.js
@@ -1,32 +1,32 @@
 const { expect } = require('chai');
 const { getArgument } = require('./utilities');
 
-describe('Utilities:', () => {
-  describe('getArgument:', () => {
+describe('\nUtilities:', () => {
+  describe('\n - getArgument:\n', () => {
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('- Should return the second word when asked for the first parameter', () => {
+    it(' - Should return the second word when asked for the first parameter', () => {
       let firstArg = getArgument('command argument ', 1);
       expect(firstArg).to.eq('argument');
     });
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('- Should return the third word when asked for the first parameter', () => {
+    it(' - Should return the third word when asked for the first parameter', () => {
       let secondArg = getArgument('command argument1 argument2', 2);
       expect(secondArg).to.eq('argument2');
     });
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('- Should return undefined when asked for a third parameter', () => {
+    it(' - Should return undefined when asked for a third parameter', () => {
       let thirdArg = getArgument('command argument1 argument2', 'argument3', 3);
       expect(thirdArg).to.be.undefined;
     });
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('- Should return undefined when asked for no parameters', () => {
+    it(' - Should return undefined when asked for no parameters', () => {
       let thirdArg = getArgument('command argument1', 3);
       expect(thirdArg).to.be.undefined;
     });
--- app/models/Cart.spec.js
+++ app/models/Cart.spec.js
@@ -1,8 +1,8 @@
 const { expect } = require('chai');
 const Cart = require('./Cart');
 
-describe('Cart:', () => {
-  describe('addItem:', () => {
+describe('\n Cart:', () => {
+  describe('\n - addItem:\n', () => {
     let cart, item;
     beforeEach(() => {
       cart = new Cart();
@@ -11,24 +11,22 @@ describe('Cart:', () => {
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('Should only have one item after adding one item', () => {
+    it(' - Should only have one item after adding one item', () => {
       cart.addItem(item, 1);
-      expect(cart.lineItems).to.have.lengthOf(1);
-      expect(cart.lineItems[0].quantity).to.eq(1);
+      expect(cart.lineItems).to.eql([{ quantity: 1, part: {} }]);
     });
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('Should have as many items as added', () => {
+    it(' - Should have as many items as added', () => {
       cart.addItem(item, 1);
       cart.addItem(item, 1);
-      expect(cart.lineItems).to.have.lengthOf(1);
-      expect(cart.lineItems[0].quantity).to.eq(2);
+      expect(cart.lineItems).to.eql([{ quantity: 2, part: {} }]);
     });
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('Should add quantities together wehn addItem is called ', () => {
+    it(' - Should add quantities together wehn addItem is called ', () => {
       cart.addItem(item, 2);
       cart.addItem(item, 4);
       expect(cart.lineItems).to.have.lengthOf(1);
@@ -37,7 +35,7 @@ describe('Cart:', () => {
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('Should add quantities together wehn addItem is called ', () => {
+    it(' - Should add quantities together wehn addItem is called ', () => {
       let item2 = {};
       cart.addItem(item, 1);
       cart.addItem(item2, 1);
@@ -46,9 +44,8 @@ describe('Cart:', () => {
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-    it('Should add quantities together wehn addItem is called ', () => {
+    it(' - Should add quantities together wehn addItem is called ', () => {
       let item2 = {};
-      let item3 = {};
       cart.addItem(item, 1);
       cart.addItem(item2, 1);
       cart.addItem(item, 3);
@@ -59,26 +56,79 @@ describe('Cart:', () => {
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   });
 
-  describe('getTotalCost:', () => {
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  describe('\n  - getTotalCost:\n', () => {
     let cart;
     beforeEach(() => {
       cart = new Cart();
       item = {};
     });
-    it('Should be  0 with no items', () => {
+    it(' - Should be 0 with no items', () => {
       expect(cart.getTotalCost()).to.eq(0);
     });
-    it('Should be  5 with a single item in cart', () => {
+    it(' - Should be 5 with a single item in cart', () => {
       let item = { cost: 5, name: 'TUKI' };
       cart.addItem(item, 1);
       expect(cart.getTotalCost()).to.eq(5);
     });
-    it('Should be  15 with multiple items in cart', () => {
-      let item = { cost: 5, name: 'TUKI' };
-      let item2 = { cost: 10, name: 'TUKI' };
+    it(' - Should be 15 with multiple items in cart', () => {
+      let item = { cost: 5 };
+      let item2 = { cost: 10 };
       cart.addItem(item, 1);
       cart.addItem(item2, 1);
       expect(cart.getTotalCost()).to.eq(15);
     });
+
+    describe('\n  - getTotalCost (variations):\n', () => {
+      const item1 = { cost: 5 };
+      const item2 = { cost: 10 };
+      const emptyLineItems = [];
+      const singleItemLineItems = [{ part: item1, quantity: 1 }];
+      const multipleLineItems = [
+        { part: item1, quantity: 1 },
+        { part: item2, quantity: 1 },
+      ];
+      let testVariations = [
+        { lineItems: emptyLineItems, expected: 0 },
+        { lineItems: singleItemLineItems, expected: 5 },
+        { lineItems: multipleLineItems, expected: 15 },
+      ];
+
+      testVariations.forEach((test) => {
+        it(` - Correctly calculates the total costwith ${test.lineItems.length} items`, () => {
+          cart.lineItems = test.lineItems;
+          expect(cart.getTotalCost()).to.eq(test.expected);
+        });
+      });
+    });
   });
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  describe('\n - empty :\n', () => {
+    let cart, item;
+    beforeEach(() => {
+      cart = new Cart();
+      item = {};
+    });
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    it(' - Should have an empty array after calling empty', () => {
+      cart.lineItems = [{}, {}, {}];
+      cart.empty();
+      expect(cart.lineItems).to.eql([]);
+    });
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    it(' - Should have an empty array after calling empty', () => {
+      let originalLineItems = cart.lineItems;
+      cart.empty();
+      expect(cart.lineItems).to.not.eq(originalLineItems);
+    });
+  });
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 });
